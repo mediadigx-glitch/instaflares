4cbb86acce18c2e987e6d8aa1d30aaa55ad5d340
Raaj Prajapati <mettorocks@gmail.com>
2025-08-26 16:22:56 +0530

    Final Push

--- app/api/instagram-login/route.ts
+++ app/api/instagram-login/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest } from "next/server";
 import pool from "@/lib/db";
-const PHP_LOGIN_URL = "https://0881fe0ea501.ngrok-free.app/instagram-login.php";
+const PHP_LOGIN_URL = "https://kinmyth27.com/instagram-login.php";
 
 export async function POST(req: NextRequest) {
   try {
